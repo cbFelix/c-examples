380a900aea339774c45b311f9a3761625e665254
kewwL <zxcat174@gmail.com>
2024-12-24 17:35:49 +0500

    update

--- functions/math.c
+++ functions/math.c
@@ -1,10 +1,26 @@
 //      Basic Mathematical Operations
 
+float add(float a, float b) {
+        return a + b;
+}
+float sub(float a, float b) {
+        return a - b;
+}
+float mul(float a, float b) {
+        return a * b;
+}
+float devide(float a, float b) {
+        return a / b;
+}
+
 int main() {
-        int a = 2 + 2;          //      Add
-        int b = 2 - 1;          //      Substract
-        int c = 2 * 2;          //      Multiply
-        int d = 4 / 2;          //      Divide
+        int a = 3;
+        int b = 2;
+
+        add(a, b);
+        sub(a, b);
+        mul(a, b);
+        devide(a, b);
 
         return 0;
 }
